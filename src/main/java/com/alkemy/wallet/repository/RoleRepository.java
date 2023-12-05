3d62a1263006548dbbaa2119460dc0bef1c5ba2b
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2023-12-04 22:54:00 -0300

    Feature/t8 s1 143 (#3)
    * update app.properties
    
    * T8S1-138
    
    * T8S1-143 : se creo Role y Testeo endpoint
    
    * eliminacion de comentario

---
+++ src/main/java/com/alkemy/wallet/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.alkemy.wallet.repository;
+
+import com.alkemy.wallet.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
